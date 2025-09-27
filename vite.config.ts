485cefd4efe5ea9d738c44eb4575bef6c82358b1
Daniel Bui <79790753+danielbui12@users.noreply.github.com>
2025-09-28 00:03:45 +0700

    fix: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/leetcode-tracking/' : '/',
+  base: '/leetcode-tracking/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
